1521988b8204581d42f419439146b0535f3ebc59
Bruno Vinícius <vbruno96@gmail.com>
2024-06-19 13:44:37 -0300

    chore: config tokens tailwindcss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,17 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Inter, IBM_Plex_Sans } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+})
+
+const ibmFlexSans = IBM_Plex_Sans({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-ibm-plex-sans',
+})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -15,8 +24,8 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className={`${inter.variable} ${ibmFlexSans.variable}`}>
+      <body className="font-inter">{children}</body>
     </html>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,40 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      pink: '#EAC5DD',
+      purple: '#9376B1',
+      blue: '#3E2682',
+      brown: '#A9948F',
+      white: '#FFF',
+      black: '#141313',
+      'off-white': '#EFECE3',
+    },
+    fontSize: {
+      xs: ['0.75rem', '1.375rem'],
+      sm: ['0.875rem', '150%'],
+      base: ['1rem', '150%'],
+      lg: ['1.125rem', '150%'],
+      xl: ['1.75rem', '3.5rem'],
+      '2xl': ['1.75rem', '3.5rem'],
+      '3xl': ['1.875rem', '3rem'],
+      '4xl': ['2.5rem', '2.5rem'],
+      '5xl': ['3rem', '3.5rem'],
+      '6xl': ['4.1875rem', '5.625rem'],
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        inter: ['var(--font-inter)'],
+        ibm: ['var(--font-ibm-plex-sans)'],
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
